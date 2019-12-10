318d353e09df415b7ad816c296c99e0509f82cf4
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2019-12-10 14:33:21 +0900

    Seed a few users / allow empty blog_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = ['toto', 'titi', 'tutu'].map do |username|
+  User.create(
+    username: username,
+    email: "#{username}@invalid.com",
+    password: "#{username}pass")
+end
+
+fake_contents = 'eaiouy'.chars.map { |c| (["bl#{c}"*3]*3).join(' ') }
+
+500.times do
+  post = Post.create(
+    content: fake_contents.sample(3).join(' '),
+    user: users.sample(1).first)
+  post.update_attribute :updated_at, (rand*30).days.ago
+end
