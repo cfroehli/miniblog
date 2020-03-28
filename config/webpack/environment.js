55c9a93b3e0c1bc927af4afa339be8942b30ede9
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2020-03-29 01:40:21 +0900

    Chunk js/css assets

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,8 +5,30 @@ environment.plugins.append(
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
+    jquery: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
 }));
+
+
+environment.config.merge({
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          }
+        }
+      }
+    }
+  }
+});
 
 module.exports = environment;
