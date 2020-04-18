77fd8a93c8cfca9e39bbcf351b18b1d56da3a743
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2020-04-19 01:10:29 +0900

    Disable gpu/shm/sandbox on chrome containers

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -5,11 +5,16 @@ require 'test_helper'
 if ENV['USE_SELENIUM_CONTAINERS'].present?
   class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     include Devise::Test::IntegrationHelpers
-
+    capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+      chromeOptions: { args: %w[no-sandbox disable-gpu disable-dev-shm-usage] }
+    )
     driven_by :selenium,
               using: :headless_chrome,
               screen_size: [1024, 800],
-              options: { url: 'http://selenium-server:4444/wd/hub' }
+              options: {
+                url: 'http://selenium-server:4444/wd/hub',
+                desired_capabilities: capabilities
+              }
 
     def setup
       Capybara.server_host = '0.0.0.0'
