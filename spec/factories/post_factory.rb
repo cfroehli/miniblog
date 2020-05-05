df6111481114a489edfef7f01ed9ee909f0b5fb9
Cyrille Froehlich <cfroehli@users.noreply.github.com>
2020-05-06 01:33:25 +0900

    Drop minitest files

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -2,5 +2,13 @@ FactoryBot.define do
   factory :post do
     sequence(:content) { |n| "Content of post[#{n}] from user[#{user.username}]" }
     association :user
+
+    trait :with_author do
+      user_name { user.username }
+    end
+
+    factory :post_with_author do
+      with_author
+    end
   end
 end
